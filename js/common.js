7f3dd9d6ff965d87d511593f210ebc8509325f54
Victor Powell <vicapow@gmail.com>
2014-11-06 17:22:25 -0800

    attempt two at bug fix.

--- js/common.js
+++ js/common.js
@@ -1,10 +1,11 @@
 
 
 var utils = {
-  setFBIframeLink: function($scope, $sce, $window) {
+  setCommonScope: function($scope, $sce, $window) {
     $scope.fbIframeLink = $sce.trustAsResourceUrl(
       '//www.facebook.com/plugins/share_button.php?href='
       + encodeURIComponent($window.location.href)
       + '&width&layout=button_count')
+    $scope.basepath = '/explained-visually/'
   }
 }
