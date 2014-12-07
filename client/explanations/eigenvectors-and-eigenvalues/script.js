0a138245081e075e8327c6bf5868c2114722aeb1
Victor Powell <vicapow@gmail.com>
2014-12-07 12:59:20 -0800

    before changing matrixA.

--- client/explanations/eigenvectors-and-eigenvalues/script.js
+++ client/explanations/eigenvectors-and-eigenvalues/script.js
@@ -1,5 +1,14 @@
 'use strict'
 
+var myApp = angular.module('myApp', ['ev'])
+
+myApp.controller('MainCtrl', function($scope) {
+  $scope.opts = {
+    matrixA: [ [1, 0], [0, 1] ],
+    sampleVector: [1, 1],
+  }
+})
+
 var color = {
     primary: '#3498db'
   , secondary: '#2ecc71'
@@ -12,6 +21,7 @@ var format = d3.format('.1f')
 var domainL = 2
 var xTicks = [-2, -1, 1, 2]
 var yTicks = xTicks
+var nobR = 10
 
 var origin = function() { return [0, 0] }
 var dash = '2, 2'
@@ -49,7 +59,45 @@ function addMarkers(defs) {
   markers.append('path').attr('d', 'M 0,0 V4 L2,2 Z')
 }
 
-var myApp = angular.module('myApp', ['ev'])
+function basisConstraint(pos) {
+  if (pos[0] < 0) pos[0] = 0
+  if (pos[1] < 0) pos[1] = 0
+  if (pos[0] > 2) pos[0] = 2
+  if (pos[1] > 2) pos[1] = 2
+  return pos
+}
+
+function sampleConstraint(pos) {
+  if (pos[0] > 2) pos[0] = 2
+  else if (pos[0] < -2) pos[0] = -2
+  if (pos[1] > 2) pos[1] = 2
+  else if (pos[1] < -2) pos[1] = -2
+  return pos
+}
+
+
+function drag(g, vec, constraint, cn, scope, stage, x, y) {
+  function beginHighlight() {
+    scope.$apply(function(){ scope.opts.highlight = cn })
+  }
+  function endHighlight() {
+    scope.$apply(function(){ scope.opts.highlight = null })
+  }
+  g.on('mouseenter', beginHighlight)
+  g.call(d3.behavior.drag()
+    .on('dragstart', beginHighlight)
+    .on('drag', function() {
+      scope.$apply(function() {
+        var pos = d3.mouse(stage.node()) // Position in pixels.
+        pos = [x.invert(pos[0]), y.invert(pos[1])] // In base coords.
+        if (constraint) pos = constraint(pos)
+        vec[0] = pos[0], vec[1] = pos[1]
+      })
+    })
+    .on('dragend', endHighlight)
+  )
+  g.on('mouseleave', endHighlight)
+}
 
 function derivedState(opts) {
   var m = matrix(opts.matrixA)
@@ -79,17 +127,33 @@ function derivedState(opts) {
   }
 }
 
-myApp.controller('MainCtrl', function($scope) {
-  $scope.opts = {
-    matrixA: [
-      [1, 0],
-      [0, 1]
-    ],
-    sampleVector: [1, 1],
-    matrixB: [ [1], [1] ],
-    matrixC: [ [1], [1] ]
-  }
-})
+function addVectors(g, data) {
+  var vectors = g.append('g').attr('class', 'vectors')
+    .selectAll('g.vector').data(data)
+      .enter().append('g').attr('class', 'vector')
+
+  vectors.append('line')
+    .attr({
+      'marker-end': function(d) {
+        return d.head === false ? null : 'url(#vector-head-' + d.style + ')'
+      }
+      , stroke: function(d) { return color[d.style] }
+      , 'stroke-dasharray': function(d) { return d.dash }
+      , 'stroke-width': function(d) { return d['stroke-width'] }
+    })
+  return vectors
+}
+
+function updateVectors(vectors, data, o) {
+  data.forEach(function(d) { d._p1 = d.p1(o), d._p2 = d.p2(o) })
+  vectors.select('line')
+    .attr({
+        x1: function(d) { return d._p1[0] }
+      , y1: function(d) { return d._p1[1] }
+      , x2: function(d) { return d._p2[0] }
+      , y2: function(d) { return d._p2[1] }
+    })
+}
 
 myApp.directive('matrixAsImageTransform', function() {
   function link(scope, el, attr) {
@@ -115,6 +179,10 @@ myApp.directive('matrixAsImageTransform', function() {
 
     var coord = plots.append('g').attr('class', 'coord')
 
+    var plot1 = svg.select('.plot:nth-child(1)')
+    var plot2 = svg.select('.plot:nth-child(2)')
+    var plot3 = svg.select('.plot:nth-child(3)')
+
     coord.append('g').attr('class', 'axis axis-x')
       .call(d3.svg.axis().scale(x).tickValues(xTicks).tickFormat(tickFormat))
 
@@ -122,34 +190,81 @@ myApp.directive('matrixAsImageTransform', function() {
       .call(d3.svg.axis().scale(y).orient('left').tickValues(yTicks)
         .tickFormat(tickFormat))
 
-    var originalImage = svg.select('.plot:nth-child(1)').append('g')
+    var originalImage = plot1.append('g')
     var imageS = x(1)
     originalImage.append('image')
       .attr('transform', 'translate(0, -' + imageS + ')')
       .attr({ width: imageS, height: imageS, 'xlink:href': 'resources/lenna.png' })
+      .style('opacity', '0.6')
 
-    var image = svg.select('.plot:nth-child(2)').append('g')
+    var image = plot3.append('g')
     image.append('image')
       .attr('transform', 'scale(1, -1) translate(0, -' + imageS + ')')
       .attr({ width: imageS, height: imageS, 'xlink:href': 'resources/lenna.png' })
+      .style('opacity', '0.6')
+
 
     svg.append('defs').call(addMarkers)
+    
+    var pixels = function(v) { return [x(v.x), y(v.y)] }
 
-    var plot1Vectors = [{
+    var plot1VectorData = [{
       name: 'sample'
       , p1: origin
       , p2: function(o) { return o.sample.to(pixels) }
       , style: 'tertiary'
+      , 'stroke-width': 4
     }]
 
+    var plot1Vectors = addVectors(plot1, plot1VectorData)
+
+    var plot3VectorData = [{
+      name: 'sample-transformed'
+      , p1: origin
+      , p2: function(o) { return o.st.to(pixels) }
+      , style: 'quaternary'
+      , 'stroke-width': 4
+    }]
+
+    var plot3Vectors = addVectors(plot3, plot3VectorData)
+
+    var samplePoint = plot1.append('g').attr('class', 'point')
+    samplePoint.append('circle').attr('r', 4)
+    samplePoint.append('text')
+      .attr('fill', color.tertiary)
+      .attr('transform', 'translate(10, 0)')
+
+
+    var sampleTransformPoint = plot3.append('g').attr('class', 'point')
+    sampleTransformPoint.append('circle').attr('r', 4)
+    sampleTransformPoint.append('text')
+      .attr('fill', color.quaternary)
+      .attr('transform', 'translate(10, 0)')
+
+    var sampleNob = plot1.append('circle')
+      .attr('class', 'nob')
+      .attr('r', nobR)
+      .call(drag, scope.opts.sampleVector, sampleConstraint, 'sample', scope, plot1, x, y)
+
+    var basis1Nob = plot2.append('circle')
+      .attr('class', 'nob')
+      .attr('r', nobR)
+      .call(drag, scope.opts.matrixA[0], sampleConstraint, 'b1', scope, plot2, x, y)
+
+    function transform(v) { return 'translate(' + [x(v.x), y(v.y)] + ')'}
     function redraw() {
-      m = matrix(scope.opts.matrixA)
-      var mt = [
-        [ m[0][0], m[1][0], 0],
-        [ -m[0][1], -m[1][1], 0],
-        [ 0, 0, 1]
-      ]
-      image.attr('transform', matToTrans(mt) )
+      var o = derivedState(scope.opts)
+      image.attr('transform', matToTrans(o.transformMatrix))
+      updateVectors(plot1Vectors, plot1VectorData, o)
+      updateVectors(plot3Vectors, plot3VectorData, o)
+      sampleNob.attr({cx: x(o.sample.x), cy: y(o.sample.y)})
+      samplePoint.attr('transform', transform(o.sample))
+      basis1Nob.attr('transform', transform(o.b1))
+      samplePoint.select('text')
+        .text('(x\u2080=' + format(o.sample.x) + ', y\u2080=' + format(o.sample.y) + ')')
+      sampleTransformPoint.attr('transform', transform(o.st))
+      sampleTransformPoint.select('text')
+        .text('(x\u2081=' + format(o.st.x) + ', y\u2081=' + format(o.st.y) + ')')
     }
 
     scope.$watch('opts', function() {
@@ -282,33 +397,18 @@ myApp.directive('matrixAsMapping', function() {
 
     // vectorData = eigens.concat(vectorData)
 
-    var vectors = coord.append('g').attr('class', 'vectors')
-      .selectAll('g.vector').data(vectorData)
-        .enter().append('g').attr('class', 'vector')
-    
-    vectors.append('line')
-      .attr({
-        'marker-end': function(d) {
-          return d.head === false ? null : 'url(#vector-head-' + d.style + ')'
-        }
-        , stroke: function(d) { return color[d.style] }
-        , 'stroke-dasharray': function(d) { return d.dash }
-        , 'stroke-width': function(d) { return d['stroke-width'] }
-
-      })
-
+    var vectors = addVectors(coord, vectorData)
     defs.call(addMarkers)
 
-    var nobR = 10
     var nob1 = coord.append('circle')
       .attr('class', 'nob')
       .attr('r', nobR)
-      .call(drag, opts.matrixA[0], basisConstraint, 'b1')
+      .call(drag, opts.matrixA[0], basisConstraint, 'b1', scope, stage, x, y)
 
     var nob2 = coord.append('circle')
       .attr('class', 'nob')
       .attr('r', nobR)
-      .call(drag, opts.matrixA[1], basisConstraint, 'b2')
+      .call(drag, opts.matrixA[1], basisConstraint, 'b2', scope, stage, x, y)
 
     var samplePoint = coord.append('g')
       .attr('class', 'point')
@@ -327,7 +427,7 @@ myApp.directive('matrixAsMapping', function() {
     var sampleNob = coord.append('circle')
       .attr('class', 'nob')
       .attr('r', nobR)
-      .call(drag, opts.sampleVector, sampleConsraint, 'sample')
+      .call(drag, opts.sampleVector, sampleConstraint, 'sample', scope, stage, x, y)
 
     function transform(v) { return 'translate(' + [x(v.x), y(v.y)] + ')'}
 
@@ -335,15 +435,7 @@ myApp.directive('matrixAsMapping', function() {
       
       var o = derivedState(scope.opts)
 
-      // Update the lines.
-      vectorData.forEach(function(d) { d._p1 = d.p1(o), d._p2 = d.p2(o) })
-      vectors.select('line')
-        .attr({
-            x1: function(d) { return d._p1[0] }
-          , y1: function(d) { return d._p1[1] }
-          , x2: function(d) { return d._p2[0] }
-          , y2: function(d) { return d._p2[1] }
-        })
+      updateVectors(vectors, vectorData, o)
 
       // Update the nob positions.
       nob1.attr({cx: x(o.b1.x), cy: y(o.b1.y)})
@@ -364,47 +456,6 @@ myApp.directive('matrixAsMapping', function() {
     function equationVector(g, vec) {
       g.select('.top .value').text(format(vec.x, 1))
       g.select('.bottom .value').text(format(vec.y, 1))
-    }
-
-    function drag(g, vec, constraint, cn) {
-      function beginHighlight() {
-        equation.classed('dim', true)
-        equation.classed(cn, true)
-      }
-      function endHighlight() {
-        equation.classed('dim', false)
-        equation.classed(cn, false)
-      }
-      g.on('mouseenter', beginHighlight)
-      g.call(d3.behavior.drag()
-        .on('dragstart', beginHighlight)
-        .on('drag', function() {
-          scope.$apply(function() {
-            var pos = d3.mouse(stage.node()) // Position in pixels.
-            pos = [x.invert(pos[0]), y.invert(pos[1])] // In base coords.
-            if (constraint) pos = constraint(pos)
-            vec[0] = pos[0], vec[1] = pos[1]
-          })
-        })
-        .on('dragend', endHighlight)
-      )
-      g.on('mouseleave', endHighlight)
-    }
-
-    function basisConstraint(pos) {
-      if (pos[0] < 0) pos[0] = 0
-      if (pos[1] < 0) pos[1] = 0
-      if (pos[0] > 2) pos[0] = 2
-      if (pos[1] > 2) pos[1] = 2
-      return pos
-    }
-
-    function sampleConsraint(pos) {
-      if (pos[0] > 2) pos[0] = 2
-      else if (pos[0] < -2) pos[0] = -2
-      if (pos[1] > 2) pos[1] = 2
-      else if (pos[1] < -2) pos[1] = -2
-      return pos
     }
 
     var equation = el.append('div').attr('class', 'equation')
@@ -459,156 +510,12 @@ myApp.directive('matrixAsMapping', function() {
       redraw()
     }, true)
 
-  }
-  return {link: link, restrict: 'E'}
-})
+    scope.$watch('opts.highlight', function(highlight, prevHighlight) {
+      if (prevHighlight) equation.classed(prevHighlight, false)
+      if (highlight) equation.classed('dim', true).classed(highlight, true)
+      else equation.classed('dim', false).classed(highlight, false)
+    })
 
-myApp.directive('playground', function() {
-  function link(scope, el, attr) {
-    el = d3.select(el[0])
-    var w = el.node().clientWidth, h = el.node().clientHeight
-    var svg = el.append('svg').attr({width: w, height: h})
-    var stage = svg.append('g')
-      .attr('transform', 'translate(' + [w / 2, h / 2] + ')')
-
-    var cW = 40, cH = 30, p = 50
-    
-    var matrixA = stage.append('g').attr('class', 'matrix a')
-      .call(createMatrix, scope.opts.matrixA)
-
-    var matrixB = stage.append('g').attr('class', 'matrix b')
-      .call(createMatrix, scope.opts.matrixB)
-
-    var matrixC = stage.append('g').attr('class', 'matrix c')
-      .call(createMatrix, scope.opts.matrixC)
-
-    var tW = cW * 2 + p + cW * 1 + p + cW * 1, yOff = 0 // -cH * 2 / 2
-    matrixA.attr('transform', 'translate(' + [-tW / 2, yOff] + ')')
-    matrixB.attr('transform', 'translate(' + [-tW / 2 + cW * 2 + p, yOff] + ')')
-    matrixC.attr('transform', 'translate(' + [-tW / 2 + cW * 2 + p + cW * 1 + p, yOff] + ')')
-
-    function createMatrix(g, data) {
-      g.selectAll('.row').data(data)
-        .enter().append('g').attr('class', 'row')
-        .attr('transform', function(d, i) {
-          return 'translate(' + [0, i * cH] + ')'
-        })
-        .selectAll('.col').data(function(d) { return d})
-          .enter().append('g').attr('class', 'col')
-          .attr('transform', function(d, i) {
-            return 'translate(' + [i * cW, 0] + ')'
-          })
-          .append('text').text(function(d) { return d})
-          .attr('x', cW / 2).attr('y', cH / 2 + 4)
-      var c = 0.2
-      var lOutline = [[cW * c, 0], [0, 0], [0, cH], [0, cH * 2], 
-        [cW * c, cH * 2]]
-      g.append('path').attr('class', 'outline')
-        .attr('d', 'M' + lOutline.join('L'))
-      var rOutline = lOutline
-        .map(function(p) { return [-p[0] + cW * data[0].length, p[1]] })
-      g.append('path').attr('class', 'outline')
-        .attr('d', 'M' + rOutline.join('L'))
-    }
-
-    function updateMatrix(g, data) {
-      g.selectAll('.row').data(data)
-        .selectAll('.col').data(function(d) { return d})
-        .select('text').text(function(d) { return d3.round(d, 2)})
-    }
-
-    var pW = 100, pH = 100
-    var baseX = d3.scale.linear().domain([-1.5, 1.5]).range([-pW / 2, pW / 2])
-    var baseY = d3.scale.linear().domain([-1.5, 1.5]).range([pH / 2, -pH / 2])
-
-    var plotA = stage.append('g').attr('class', 'plot a')
-      .attr('transform', 'translate(' + [-87, -65] + ')')
-      .call(createPlot, scope.opts.matrixA)
-
-    var plotB = stage.append('g').attr('class', 'plot b')
-      .attr('transform', 'translate(' + [25, -65] + ')')
-      .call(createPlot, scope.opts.matrixB, true)
-
-    var plotC = stage.append('g').attr('class', 'plot c')
-      .attr('transform', 'translate(' + [125, -65] + ')')
-      .call(createPlot, scope.opts.matrixC, true, true)
-
-    function createPlot(g, matrix, isVector, hideNob) {
-      var tickFormat = function(d) { return d3.round(d) }
-
-      g.append('g').attr('class', 'axis axis-x')
-        .call(d3.svg.axis().scale(baseX).tickValues([-1, 1]).tickFormat(tickFormat))
-
-      g.append('g').attr('class', 'axis axis-y')
-        .call(d3.svg.axis().scale(baseY).orient('left').tickValues([-1, 1])
-          .tickFormat(tickFormat))
-      
-      var vecB1, vecB2
-
-      vecB1 = g.append('line').attr({class: 'basis basis-1', x1: 0, y2: 0})
-      if (!isVector)
-        vecB2 = g.append('line').attr({class: 'basis basis-2', x1: 0, y2: 0})
-
-      var nobB1, nobB2
-      if (!hideNob) {
-        nobB1 = g.append('g').datum(0)
-        nobB1.append('circle')
-          .attr('class', 'nob nob-1').attr('r', 4)
-        if (!isVector) {
-          nobB2 = g.append('g').datum(1)
-          nobB2.append('circle')
-            .attr('class', 'nob nob-2').attr('r', 4)
-        }
-
-        var drag = d3.behavior.drag()
-          .on('drag', function(d, i) { scope.$apply(function() {
-            var pos = d3.mouse(g.node()) // In pixels.
-            pos = [baseX.invert(pos[0]), baseY.invert(pos[1])] // In base coords.
-            if (isVector) matrix[0][0] = pos[0], matrix[1][0] = pos[1]
-            else {
-              matrix[0][d] = pos[0]
-              matrix[1][d] = pos[1]
-            }
-          }.bind(this))})
-        nobB1.call(drag)
-        if (!isVector) nobB2.call(drag)
-      }
-    }
-
-    function updatePlot(g, matrix, isVector) {
-      if (!isVector) {
-        // Matrix
-        var b1 = [ matrix[0][0], matrix[1][0] ]
-        b1 = [baseX(b1[0]), baseY(b1[1])]
-        g.select('.basis-1').attr({ x2: b1[0], y2: b1[1] })
-        g.select('.nob-1').attr('transform', 'translate(' + b1 + ')')
-
-        var b2 = [ matrix[0][1], matrix[1][1] ]
-        b2 = [baseX(b2[0]), baseY(b2[1])]
-        g.select('.basis-2').attr({ x2: b2[0], y2: b2[1] })
-        g.select('.nob-2').attr('transform', 'translate(' + b2 + ')')
-      } else {
-        // Column Vector
-        var b1 = [ matrix[0][0], matrix[1][0] ]
-        b1 = [baseX(b1[0]), baseY(b1[1])]
-        g.select('.basis-1').attr({ x2: b1[0], y2: b1[1] })
-        g.select('.nob-1').attr('transform', 'translate(' + b1 + ')')
-      }
-    }
-
-    function update() {
-      var o = scope.opts, A = o.matrixA, B = o.matrixB, C = o.matrixC
-      C = matrix(A).multi(matrix(B))
-      plotA.call(updatePlot, A)
-      matrixA.call(updateMatrix, A)
-      plotB.call(updatePlot, B, true)
-      matrixB.call(updateMatrix, B)
-      plotC.call(updatePlot, C, true)
-      matrixC.call(updateMatrix, C)
-    }
-
-    scope.$watch('opts', update, true)
-    update()
   }
   return {link: link, restrict: 'E'}
 })
