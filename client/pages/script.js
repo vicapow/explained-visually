8c1368bf6bab93623d8e48368fac6826789f8288
Victor Powell <vicapow@gmail.com>
2015-02-12 09:27:35 -0800

    Update PCA thumb.

--- client/pages/script.js
+++ client/pages/script.js
@@ -438,3 +438,33 @@ myApp.directive('ev6Thumb', function() {
   }
   return { link: link, restrict: 'E' }
 })
+
+myApp.directive('ev9Thumb', function() {
+  function link(scope, el, attr) {
+    var sel = d3.select(el[0])
+    var w = sel.node().clientWidth, h = sel.node().clientHeight
+    var svg = sel.append('svg').attr({width: w, height: h})
+    var m = { l: 220, t: 10, r: 10, b: 10 }
+    var g = svg.append('g').attr('transform', 'translate(' + [ w * 0.5, h * 0.7] + ')')
+    var color = { primary: '#e74c3c', secondary: '#2ecc71' }
+    var s = g.append('g').attr('transform', 'scale(25) translate(0, 0) rotate(-45)')
+    var dur = 500, r = 60
+    s.append('line').attr({x1: 0, x2: r, y1: 0, y2: 0})
+      .style('stroke-width', 4).style('stroke', color.primary)
+    s.append('line').attr({x1: 0, x2: 0, y1: 0, y2: -r})
+      .style('stroke-width', 4).style('stroke', color.secondary)
+    svg.on('mouseenter', function() {
+      s
+        .transition()
+        .duration(dur)
+        .attr('transform', 'scale(1) rotate(0) ')
+    })
+    .on('mouseleave', function() {
+      s
+        .transition()
+        .duration(dur)
+        .attr('transform', 'scale(25) translate(0, 0) rotate(-45)')
+    })
+  }
+  return { link: link, restrict: 'E' }
+})
